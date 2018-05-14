7dccbef416ca0300034c7cc4151816df438ce614
lukasbernert <lukas@bernert.at>
2018-05-14 19:26:55 +0200

    Drag and drop styling

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -9,27 +9,6 @@ import {Column} from "./column/column.model";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styles: [`
-    div.scroll-list {
-      overflow: auto;
-      max-height: 70vh;
-    }
-
-    .drag-over {
-      border: #ff525b dashed 2px;
-    }
-
-    .drag-hint {
-      border: #ffc100 dashed 2px;
-      /*transition: all .2s ease-in-out;*/
-      /*transform: scale(1.05);*/
-    }
-
-    .drag-target-border {
-      border: #00bfff dashed 2px;
-    }
-  `
-  ]
 })
 export class AppComponent {
   tasks: Task[];
@@ -37,40 +16,103 @@ export class AppComponent {
 
   constructor(private getcolumnsService: GetcolumnsserviceService){
   }
-
-  list1 = [
-    {title: 'Toyota', description: 'bla bla', assignee: 'lukas', estimate: 10},
-    {title: 'Bugati', description: 'ble ble', assignee: 'lukas', estimate: 5},
-    {title: 'sfdgf', description: 'cwecewcwbla bla', assignee: 'lukas', estimate: 10},
-    {title: 'Bfdg', description: 'blcwewcwere ble', assignee: 'luerretkas', estimate: 5},
-    {title: 'Toxfgc', description: 'bla blwewecwewa', assignee: 'lukacecerws', estimate: 10},
-    {title: 'Bdsfdgf', description: 'ble blsretrwree', assignee: 'lukceeas', estimate: 5},
-    {title: 'Suzuki', description: 'blu blu', assignee: 'martin', estimate: 3}
+/*
+  listA = [
+    {name: 'milk'},
+    {name: 'juice'}
   ];
 
-  list2 = [
-    {title: 'lala', description: 'bla bla', assignee: 'matthias', estimate: 10},
-    {title: 'test', description: 'ble ble', assignee: 'max', estimate: 5},
-    {title: 'title', description: 'blu blu', assignee: 'maxime', estimate: 3}
+  listB = [
+    {name: 'beer'},
+    {name: 'water'}
   ];
 
+  listC = [
+    {name: 'wine'}
+  ];
 
-  onList1Drop(e: DropEvent) {
-    this.list1.push(e.dragData);
-    this.removeItem(e.dragData, this.list2)
+  onListADrop(e: DropEvent) {
+    this.listA.push(e.dragData);
+    this.removeItem(e.dragData, this.listB);
   }
 
-  onList2Drop(e: DropEvent) {
-    this.list2.push(e.dragData);
-    this.removeItem(e.dragData, this.list1)
+  onListBDrop(e: DropEvent) {
+    this.listB.push(e.dragData);
+    this.removeItem(e.dragData, this.listA);
   }
 
+  onListCDrop(e: DropEvent) {
+    this.listC.push(e.dragData);
+    this.removeItem(e.dragData, this.listA);
+  }
+*/
   removeItem(item: any, list: Array<any>) {
     let index = list.map(function (e) {
       return e.title
     }).indexOf(item.title);
     list.splice(index, 1);
   }
+
+  list1 = [
+    {title: 'Task Title', description: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam.', assignee: 'lukas', estimate: 10, columnId: 1},
+    {title: 'randomtitle', description: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam.', assignee: 'lukas', estimate: 5, columnId: 1},
+    {title: 'anothertask', description: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam.', assignee: 'lukas', estimate: 10, columnId: 1},
+    {title: 'onemoretask title', description: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam.', assignee: 'luerretkas', estimate: 5, columnId: 1},
+    {title: 'yet another task', description: 'Lorem ipsum dolor sit amet.', assignee: 'lukacecerws', estimate: 10, columnId: 1},
+    {title: 'Bdsfdgf', description: 'Lorem ipsum dolor sit amet.', assignee: 'lukceeas', estimate: 5, columnId: 1},
+    {title: 'Suzuki', description: 'Lorem ipsum dolor sit amet.', assignee: 'martin', estimate: 3, columnId: 1}
+  ];
+
+  list2 = [
+    {title: 'lala', description: 'Lorem ipsum dolor sit amet.', assignee: 'matthias', estimate: 10, columnId: 2},
+    {title: 'test', description: 'Lorem ipsum dolor sit amet.', assignee: 'max', estimate: 5, columnId: 2},
+    {title: 'title', description: 'Lorem ipsum dolor sit amet.', assignee: 'maxime', estimate: 3, columnId: 2}
+  ];
+
+  list3 = [
+    {title: 'lala', description: 'Lorem ipsum dolor sit amet.', assignee: 'matthias', estimate: 10, columnId: 3}
+  ];
+
+  list4 = [];
+
+  list5 = [];
+
+
+  onList1Drop(e: DropEvent) {
+    console.log(e);
+    this.list1.unshift(e.dragData);
+    this.removeItem(e.dragData, this['list'+e.dragData.columnId]);
+    this.list1[0].columnId = 1;
+  }
+
+  onList2Drop(e: DropEvent) {
+    console.log(e);
+    this.list2.unshift(e.dragData);
+    this.removeItem(e.dragData, this['list'+e.dragData.columnId]);
+    this.list2[0].columnId = 2;
+  }
+
+  onList3Drop(e: DropEvent) {
+    console.log(e);
+    this.list3.unshift(e.dragData);
+    this.removeItem(e.dragData, this['list'+e.dragData.columnId]);
+    this.list3[0].columnId = 3;
+  }
+
+  onList4Drop(e: DropEvent) {
+    console.log(e);
+    this.list4.unshift(e.dragData);
+    this.removeItem(e.dragData, this['list'+e.dragData.columnId]);
+    this.list4[0].columnId = 4;
+  }
+
+  onList5Drop(e: DropEvent) {
+    console.log(e);
+    this.list5.unshift(e.dragData);
+    this.removeItem(e.dragData, this['list'+e.dragData.columnId]);
+    this.list5[0].columnId = 5;
+  }
+
   ngOnInit() {
     this.getColumns();
   }
